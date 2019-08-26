7c92d98331b4b6a9e38208e26514ee807ee15288
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-26 09:20:06 +0700

    add_users#edit,#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: %i(edit update)
+
   def index
     @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_path unless @user
+  end
+
+  def user_params
+    params.require(:user).permit :name, :email
   end
 end
